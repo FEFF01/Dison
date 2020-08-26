e8b0d4cd2e3187a910d588b8309d7e1c24cf5480
FEFF01 <FEFF01@hotmail.com>
2020-08-26 22:15:15 +0800

    ..

---
+++ out/72.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[72],{
+
+/***/ "./node_modules/astexplorer-go/index.js":
+/*!**********************************************!*\
+  !*** ./node_modules/astexplorer-go/index.js ***!
+  \**********************************************/
+/*! exports provided: init, parseFile */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"init\", function() { return init; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"parseFile\", function() { return parseFile; });\nlet _init = async function () {\n  const exports = await __webpack_require__.e(/*! import() */ 65).then(__webpack_require__.bind(null, /*! ./parser.wasm */ \"./node_modules/astexplorer-go/parser.wasm\"))\n  window._go.run({exports})\n}\n\nasync function init() {\n  if (_init) {\n    await _init()\n    _init = undefined\n  }\n}\n\n\nfunction parseFile (code) {\n  return JSON.parse(window.__GO_PARSE_FILE__(code))\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvYXN0ZXhwbG9yZXItZ28vaW5kZXguanMuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9ub2RlX21vZHVsZXMvYXN0ZXhwbG9yZXItZ28vaW5kZXguanM/Yjc2NCJdLCJzb3VyY2VzQ29udGVudCI6WyJsZXQgX2luaXQgPSBhc3luYyBmdW5jdGlvbiAoKSB7XG4gIGNvbnN0IGV4cG9ydHMgPSBhd2FpdCBpbXBvcnQoJy4vcGFyc2VyLndhc20nKVxuICB3aW5kb3cuX2dvLnJ1bih7ZXhwb3J0c30pXG59XG5cbmV4cG9ydCBhc3luYyBmdW5jdGlvbiBpbml0KCkge1xuICBpZiAoX2luaXQpIHtcbiAgICBhd2FpdCBfaW5pdCgpXG4gICAgX2luaXQgPSB1bmRlZmluZWRcbiAgfVxufVxuXG5cbmV4cG9ydCBmdW5jdGlvbiBwYXJzZUZpbGUgKGNvZGUpIHtcbiAgcmV0dXJuIEpTT04ucGFyc2Uod2luZG93Ll9fR09fUEFSU0VfRklMRV9fKGNvZGUpKVxufVxuIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUFBO0FBQUE7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTsiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./node_modules/astexplorer-go/index.js\n");
+
+/***/ })
+
+}]);
